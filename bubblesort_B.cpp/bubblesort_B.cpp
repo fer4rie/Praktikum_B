7c92f1652059abe54ed3b1b74ee0b3fba585a11c
fer4rie <firapuspita2106@gmail.com>
2026-03-05 17:55:46 +0700

    looping with i start from 0 until n-1

--- bubblesort_B.cpp/bubblesort_B.cpp
+++ bubblesort_B.cpp/bubblesort_B.cpp
@@ -10,14 +10,24 @@ void input()
     int d;
     while (true)
     {
-        cout << "Masukan banyaknya elemen pada array :";
-        cin >> n;
-        if (n <= 20)
-            break;
+        cout << "Masukan banyaknya elemen pada array :";    //output ke layar
+        cin >> n;                                          // input dari pengguna
+        if (n <= 20)                                       // jika n kurang dari atau sama dengan 20, maka
+            break;                                         // keluar dari loop
         else
-        {
-            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        {                                                               // jika n lebih dari 20, maka
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";    //output ke layar
         }
+    }
+    cout << endl;
+    cout << "===================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "===================" << endl;
 
+    for (int i = 0; i < n; i++)
+    {                                               // looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";      //output ke layar
+        cin >> arr[i];                              // input dari pengguna
     }
 }
+
